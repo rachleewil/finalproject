f6aaeca66b61d6ec214bd5a767f1cf8d10ea16ff
Gina Stewart <stewart_rs@yahoo.com>
2021-11-27 22:33:51 -0500

    react app tested and able to run with simple App.tsx change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import fair_640 from "../src/images/fair_640.jpg";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img id="amigo-img" src={fair_640} alt="carnival" height={400}></img>
+        <p>Carnival Coming Soon.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
