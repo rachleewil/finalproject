21d34af8d455b0c21e6629b85ec217f7250662cf
rachleewil <rachleewil@yahoo.com>
2021-12-02 18:51:54 -0500

    add nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import fair_640 from "../src/images/fair_640.jpg";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ViewFortunes from "./components/Carnival";
 import LoginForm from "./components/LoginForm";
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <h1>Welcome!</h1>
         <Routes>
           <Route path="/login" element={<LoginForm /> } />
