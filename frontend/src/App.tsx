62dc2e2bf921e03c12fcbc27e225e1de697751b1
rachleewil <rachleewil@yahoo.com>
2021-12-02 19:59:10 -0500

    add home page and move images to public folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import fair_640 from "../src/images/fair_640.jpg";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ViewFortunes from "./components/Carnival";
 import LoginForm from "./components/LoginForm";
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         <h1>Welcome!</h1>
         <Routes>
           <Route path="/login" element={<LoginForm /> } />
+          <Route path="/Home" element={<Home /> } />
           <Route path="/fortunes" element={<ViewFortunes />} />
         </Routes>
       </Router>
