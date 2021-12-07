e948ea8e8782afc3f361740962dce285f37e0f91
JessicaPreuss <82731575+JessicaPreuss@users.noreply.github.com>
2021-12-06 21:25:09 -0500

    favorites too

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,19 +8,15 @@ import Home from "./components/Home";
 import ViewDrinks from "./components/Drinks";
 import Favorites from "./components/Favorites";
 import PostFortunes from "./components/PostFortune";
-<<<<<<< HEAD
 import FavoritesList from "./components/FavoritesList";
 import FavoritesProvider from "./context/FavoritesContext";
-=======
 import AddFortuneFormRoute from "./components/routes/AllFortunesRoutes";
->>>>>>> f240386c8057c1e4f5e3ae7ab9744d2f4dc99b51
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
-<<<<<<< HEAD
         <FavoritesProvider>
           <Routes>
             <Route path="/login" element={<LoginForm />} />
@@ -29,19 +25,9 @@ function App() {
             <Route path="/drinks" element={<ViewDrinks />} />
             <Route path="/favorites" element={<FavoritesList />} />
             <Route path="/createFortunes" element={<PostFortunes />} />
+            <Route path="/addfortune" element={<AddFortuneFormRoute />} />
           </Routes>
         </FavoritesProvider>
-=======
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/fortunes" element={<ViewFortunes />} />
-          <Route path="/drinks" element={<ViewDrinks />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/createfortune" element={<PostFortunes />} />
-          <Route path="/addfortune" element={<AddFortuneFormRoute />} />
-        </Routes>
->>>>>>> f240386c8057c1e4f5e3ae7ab9744d2f4dc99b51
       </Router>
     </div>
   );
