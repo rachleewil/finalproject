5e49c617d9bce6dd86f211401633fe3b09a695ea
rachleewil <rachleewil@yahoo.com>
2021-12-15 20:58:19 -0500

    upload firebase url

--- frontend/src/services/Users.ts
+++ frontend/src/services/Users.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { Users } from "../models/Users";
 
-//const baseUrl = "http://localhost:5001/carnival-app-b84f4/us-central1/api";
 const baseUrl = "https://us-central1-carnival-app-b84f4.cloudfunctions.net/api";
+// const baseUrl = "http://localhost:5001/carnival-app-b84f4/us-central1/api";
 
 export async function fetchUser(): Promise<Users[]> {
   //hit our /topicsFortuneCookie
